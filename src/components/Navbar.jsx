eca6c37eadf374b2223573a79ab7a4beba781cd6
Laureano Hurtado <laureanohurtado1@gmail.com>
2023-10-12 12:14:26 -0500

    navmobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,49 +73,8 @@ export default function Navbar() {
               </div>
             ) : ('')}
           </div>
-
-
-
-
           {/* Navegación móvil (condicional) */}
-          {option && (
-            <div className="animate-fade-right animate-once fixed top-0 left-0  xxsm:w-[50%] xsm:w-[70%] h-full bg-gray-800 text-white z-20 rounded-r-xl overflow-y-auto md:hidden">
-            <div className="flex flex-col">
-              <svg onClick={handleMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mt-2 mr-2 self-end">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </div>
-          
-            <div className="flex flex-col items-center justify-evenly h-full p-4">
-              <Link to="/" className="hover:text-violet-800 hover:border-b-2 cursor-pointer transition duration-700 text-lg">
-                Home
-              </Link>
-              <Link to="/appointment" className="hover:text-violet-800 hover:border-b-2 transition duration-700">
-                Schedule Appointment
-              </Link>
-              <Link to="#" className="hover:text-violet-800 hover:border-b-2 transition duration-700">
-                Services
-              </Link>
-              <Link to="#" className="hover:text-violet-800 hover:border-b-2 transition duration-700">
-                About Us
-              </Link>
-              <Link to="#" className="hover:text-violet-800 hover:border-b-2 transition duration-700">
-                Contact
-              </Link>
-              {!role ? (
-                <Link to="/signin" className="hover:text-violet-800 hover:border-b-2 transition duration-700">
-                  Log In
-                </Link>
-              ) : ('')}
-              {role ? (
-                <div onClick={backHome} className="hover:text-violet-800 hover:border-b-2 transition duration-700 cursor-pointer">
-                  Log Out
-                </div>
-              ) : ('')}
-            </div>
-          </div>
-          
-          )}
+          {option && (<NavbarMobile backHome={backHome} estado={handleMenu} />)}
         </div>
       </div>
     </>
