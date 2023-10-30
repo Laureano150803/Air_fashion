380d72b7622edb0613f77d444a08f7b152c8269f
Laureano Hurtado <laureanohurtado1@gmail.com>
2023-10-30 11:53:52 -0500

    fix animation

--- src/components/CardService3D.jsx
+++ src/components/CardService3D.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
-import imgDamas from '/src/assets/images/por_silas.jpg'
+import React from 'react';
+import imgDamas from '/src/assets/images/por_silas.jpg';
 import ReactDOM from 'react-dom';
-import { Tilt } from 'react-tilt'
-import '../TiltComponent.css'
+import { Tilt } from 'react-tilt';
+import { Link } from 'react-router-dom';
+import '../TiltComponent.css';
 
 const CardService3D = () => {
+  const defaultOptions = {
+    reverse: false,
+    max: 35,
+    perspective: 1000,
+    scale: 1.1,
+    speed: 1000,
+    transition: true,
+    axis: null,
+    reset: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+  };
 
-    const defaultOptions ={
-        reverse: false,
-        max: 35,
-        perspective: 1000,
-        scale: 1.1,
-        speed: 1000,
-        transition: true,
-        axis: null,
-        reset: true,
-        easing:"cubic-bezier(.03,.98,.52,.99)"
-
-    }
-   
   return (
     <>
-    <Tilt options={defaultOptions} style={{ height: 620, width: 410, backgroundImage:'url("src/assets/images/por_silas.jpg")' }}  className='flex justify-center items-center bg-transparent'>
-        <div  className='flex justify-center items-center text-white text-4xl' data-tilt >hola mundo </div>
-    </Tilt>
-    
+      <Link to={'/register'}>
+        <Tilt
+          options={defaultOptions}
+          style={{
+            height: 620,
+            width: 410,
+            backgroundImage: 'url("https://i.pinimg.com/280x280_RS/ab/13/c9/ab13c9de74f1839af0d8464e0d91450b.jpg")',
+            transformStyle: 'preserve-3d',
+          }}
+          className='flex justify-center items-center bg-transparent'
+        >
+          <p style={{transform: 'translateZ(40px)'}} to={'/register'} className='flex justify-center items-center  text-white text-4xl' data-tilt>
+            hola mundo
+          </p>
+        </Tilt>
+      </Link>
     </>
-  )
-}
+  );
+};
 
-
-export default CardService3D
+export default CardService3D;
--- src/components/CardsHairdresser.jsx
+++ src/components/CardsHairdresser.jsx
@@ -12,20 +12,20 @@ const CardsHairdresser = () => {
 
                 <h1 className='text-4xl text-center text-gray-900 mt-10'>Our Professional Team</h1>
                 <br />
-                    <div class="line-loader w-96 h-1"></div>
+                    <div className="line-loader w-96 h-1"></div>
 
                 </div>
                 <div className="flex flex-wrap  gap-10 mt-20 ">
                 
 
-                    <div class="w-80 rounded-xl  flex flex-col justify-center items-center  shadow-md shadow-blue-400/40 ">
+                    <div className="w-80 rounded-xl  flex flex-col justify-center items-center  shadow-md shadow-blue-400/40 ">
                         <div className="  -translate-y-20 shadow mx-auto h-52 w-52   rounded-full  border-4 overflow-visible">
-                            <img class="rounded-full" src={img} />
+                            <img className="rounded-full" src={img} />
                         </div>
 
-                        <div class="px-6 py-4  flex flex-col items-end justify-end   ">
-                            <div class="font-bold h-auto text-xl ">laura rivera</div>
-                            <p class="text-gray-700 text-base">
+                        <div className="px-6 py-4  flex flex-col items-end justify-end   ">
+                            <div className="font-bold h-auto text-xl ">laura rivera</div>
+                            <p className="text-gray-700 text-base">
                                 HOLA MUNDO
                             </p>
                         </div>
@@ -33,42 +33,42 @@ const CardsHairdresser = () => {
 
                     </div>
 
-                    <div class="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
+                    <div className="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
                         <div className="  -translate-y-20 shadow mx-auto h-52 w-52  border-white rounded-full  border-4 overflow-visible">
-                            <img class="rounded-full    " src={img} />
+                            <img className="rounded-full    " src={img} />
                         </div>
 
-                        <div class="px-6 py-4  flex flex-col items-end justify-end  ">
-                            <div class="font-bold h-auto text-xl ">laura rivera</div>
-                            <p class="text-gray-700 text-base">
+                        <div className="px-6 py-4  flex flex-col items-end justify-end  ">
+                            <div className="font-bold h-auto text-xl ">laura rivera</div>
+                            <p className="text-gray-700 text-base">
                                 HOLA MUNDO
                             </p>
                         </div>
 
 
                     </div>
-                    <div class="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
+                    <div className="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
                         <div className="  -translate-y-20 shadow mx-auto h-52 w-52  border-white rounded-full  border-4 overflow-visible">
-                            <img class="rounded-full    " src={img} />
+                            <img className="rounded-full    " src={img} />
                         </div>
 
-                        <div class="px-6 py-4  flex flex-col items-end justify-end  ">
-                            <div class="font-bold h-auto text-xl ">laura rivera</div>
-                            <p class="text-gray-700 text-base">
+                        <div className="px-6 py-4  flex flex-col items-end justify-end  ">
+                            <div className="font-bold h-auto text-xl ">laura rivera</div>
+                            <p className="text-gray-700 text-base">
                                 HOLA MUNDO
                             </p>
                         </div>
 
 
                     </div>
-                    <div class="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
+                    <div className="w-80 rounded-xl bg-white flex flex-col justify-center items-center  shadow-md shadow-blue-400/40">
                         <div className="  -translate-y-20 shadow mx-auto h-52 w-52  border-white rounded-full  border-4 overflow-visible">
-                            <img class="rounded-full    " src={img} />
+                            <img className="rounded-full    " src={img} />
                         </div>
 
-                        <div class="px-6 py-4  flex flex-col items-end justify-end  ">
-                            <div class="font-bold h-auto text-xl ">laura rivera</div>
-                            <p class="text-gray-700 text-base">
+                        <div className="px-6 py-4  flex flex-col items-end justify-end  ">
+                            <div className="font-bold h-auto text-xl ">laura rivera</div>
+                            <p className="text-gray-700 text-base">
                                 HOLA MUNDO
                             </p>
                         </div>
@@ -78,7 +78,6 @@ const CardsHairdresser = () => {
 
                 </div>
             </div>
-            <br /><br /><br /><br />
 
 
         </>
