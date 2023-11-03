13cb1d4e96c946383e3609db3673b56b843ee1a5
Laureano Hurtado <109369745+Laureano150803@users.noreply.github.com>
2023-11-03 12:01:03 -0500

    Lau (#17)
    * fixing enviroment
    
    * starting front proyect
    
    * Footer
    
    * probando redux, haciendo alertas
    
    * navmobile
    
    * cita funcionando
    
    * fixing gestion de cita
    
    * fix animation
    
    * cambiando boton de citas
    
    * detail services
    
    * navbar validations
    
    * adding material tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ import LittleOfUs from "./components/LittleOfUs"
 import ImageGallery from "./components/ImageGallery"
 import CardsHairdresser from "./components/CardsHairdresser"
 import CardService3D from "./components/CardService3D"
-
+import {Card,CardHeader,CardBody,CardFooter,Typography,Button,} from "@material-tailwind/react";
 
 
 function App() {
+
+  const [services, setServices] = useState([])
+
+  useEffect(() => {
+    axios.get(apiUrl + 'services').then(res => setServices(res.data.Response)).catch(res => console.log(res))
+  }, [])
+
+  console.log(services)
 
 
   return (
@@ -34,10 +42,35 @@ function App() {
         </div>
 
       </div>
+      <div>
+      <Card className="mt-6 w-96">
+      <CardHeader color="blue-gray" className="relative h-56">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+          alt="card-image"
+        />
+      </CardHeader>
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          UI/UX Review Check
+        </Typography>
+        <Typography>
+          The place is close to Barceloneta Beach and bus stop just 2 min by
+          walk and near to &quot;Naviglio&quot; where you can enjoy the main
+          night life in Barcelona.
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <Button>Read More</Button>
+      </CardFooter>
+    </Card>
+        
+      </div>
 
 
 
-      
+
+
 
     </>
   )
