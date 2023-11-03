a6f1c31416f97fd1a922b4e6df2217e4f5152999
Laureano Hurtado <laureanohurtado1@gmail.com>
2023-11-03 11:09:51 -0500

    navbar validations

--- src/components/CardsHairdresser.jsx
+++ src/components/CardsHairdresser.jsx
@@ -10,20 +10,20 @@ const CardsHairdresser = () => {
             <div className="aboutUs bg-slate-100 bg-opacity-0  flex flex-col  gap-24  items-center  ">
                 <div className="">
 
-                <h1 className='text-4xl text-center text-gray-900 mt-10'>Our Professional Team</h1>
-                <br />
+                    <h1 className='text-4xl text-center text-gray-900 mt-10'>Our Professional Team</h1>
+                    <br />
                     <div className="line-loader w-96 h-1"></div>
 
                 </div>
                 <div className="flex flex-wrap  gap-10 mt-20 ">
-                
 
-                    <div className="w-80 rounded-xl  flex flex-col justify-center items-center  shadow-md shadow-blue-400/40 ">
-                        <div className="  -translate-y-20 shadow mx-auto h-52 w-52   rounded-full  border-4 overflow-visible">
+
+                    <div className="w-80 rounded-xl  flex flex-col  items-center  shadow-md shadow-blue-400/40 border">
+                        <div className="  -translate-y-20 shadow mx-auto h-52 w-52 rounded-full    border-4 overflow-visible">
                             <img className="rounded-full" src={img} />
                         </div>
 
-                        <div className="px-6 py-4  flex flex-col items-end justify-end   ">
+                        <div className="-translate-y-10 px-6 py-4  flex flex-col items-center justify-center">
                             <div className="font-bold h-auto text-xl ">laura rivera</div>
                             <p className="text-gray-700 text-base">
                                 HOLA MUNDO
