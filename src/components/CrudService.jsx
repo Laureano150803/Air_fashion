2dbfbb99294850b8cb5abd6c93a4ad03fdd352fb
yeison1526 <yesiontatiscampillo1526@gmail.com>
2023-11-11 13:02:17 -0500

    curd de peluqueros de la vista admin

--- src/components/CrudService.jsx
+++ src/components/CrudService.jsx
@@ -28,7 +28,7 @@ const CrudService = () => {
                 <input type="text" id="table-search-users" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users" />
               </div>
             </div>
-            <table className="w-full text-sm text-left dark:text-gray-400">
+            <table className="w-full text-sm text-left dark:text-gray-400 ">
               <thead className="text-base  uppercase bg-gray-100  dark:text-violet-600">
                 <tr>
 
@@ -49,7 +49,7 @@ const CrudService = () => {
                   </th>
                 </tr>
               </thead>
-              <tbody className='overflow-y-auto' style={{ maxHeight: '500px' }}  >
+              <tbody   >
                 {services.map((servicios) => (
 
 
