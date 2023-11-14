90d95586390a6fe4440aa68c3d191893e7126f03
R2606 <145151876+R2606@users.noreply.github.com>
2023-11-14 16:24:34 -0500

    Raul (#29)
    * crud hairdresser
    
    * Haidresser panel finished

--- src/components/HairDresserPanel.jsx
+++ src/components/HairDresserPanel.jsx
@@ -7,11 +7,19 @@ import paid from '../assets/images/paid.png'
 import dayjs from 'dayjs';
 import Swal from 'sweetalert2';
 import '../scroll.css'
+import TableOfCalendar from './TableOfCalendar';
+
 export default function HairDresserPanel() {
   const token = localStorage.getItem('token');
   const headers = { headers: { 'authorization': `Bearer ${token}` } };
 
   const [citas, setCitas] = useState([]);
+  const [filter, setFilter] = useState(false)
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
+  const handleDateChange = (newDate) => {
+    setSelectedDate(newDate);
+  };
   useEffect(() => {
     axios.get(apiUrl + 'google/allMyAppointments', headers)
       .then(res => setCitas(res.data.response))
@@ -52,14 +60,14 @@ export default function HairDresserPanel() {
     });
   }
 
+
   const doneAppointments = citas.filter(cita => cita.status === 'DONE');
   const pendingAppointments = citas.filter(cita => cita.status === 'PENDING');
 
-  console.log(citas)
 
   return (
     <>
-      <div className='flex min-h-screen w-full'>
+      <div className='flex min-h-screen w-full bg-gray-200'>
 
         <div className='flex flex-col w-[50%] custom-scrollbar' style={{ overflowY: 'auto', maxHeight: '95vh' }}>
 
@@ -127,22 +135,34 @@ export default function HairDresserPanel() {
 
         <div className='w-[50%] custom-scrollbar' style={{ overflowY: 'auto', maxHeight: '95vh' }}>
 
-          <h1 className='text-4xl text-center font-bold mb-2'>Done appointments</h1>
+          <div className='flex w-[100%] justify-center'>
+            <h1 onClick={() => setFilter(!filter)} className='text-4xl font-bold mb-2 flex items-center'>Done appointments <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 cursor-pointer hover:bg-slate-100 hover:rounded-full hover:scale-105 duration-100 ">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+            </svg></h1>
 
-          {doneAppointments.length === 0 && <div className='font-mono text-center'>You dont have Done Appointments</div>}
+
+          </div>
+          {doneAppointments.length === 0 && <div className='font-mono text-center'>You dont have Done Appointments today</div>}
+
+          {filter && <TableOfCalendar onDateChange={handleDateChange} />}
 
 
-          {doneAppointments.map(cita => (
+          {doneAppointments.filter(cita => dayjs(cita.inicio).format('MMMM/DD') === dayjs(selectedDate).format('MMMM/DD')).map(citas => (
+
             <>
-              <div key={cita._id} className="animate-fade-right animate-duration-[3000ms] p-8  border-t  dark:border-blue-200 flex gap-4">
-                <img src={cita?.servicio_id?.foto} className='h-16 w-16 bg-slate-50 rounded-3xl shadow-xl shadow-blue-200 ' />
+              <div key={citas._id} className="animate-fade-right animate-duration-[3000ms] p-8  border-t  dark:border-blue-200 flex gap-4">
+                <img src={citas?.servicio_id?.foto} className='h-16 w-16 bg-slate-50 rounded-3xl shadow-xl shadow-blue-200 ' />
                 <div className="flex flex-col gap-2">
 
                   <div className="flex gap-4 ">
-                    <h1 className='aboutUs text-xl'>{`${cita.cliente_id.nombre} ${cita.cliente_id.apellido}`}  </h1>
+                    <h1 className='aboutUs text-xl'>{`${citas.cliente_id.nombre} ${citas.cliente_id.apellido}`}  </h1>
                     <div className=" flex ">
                       <img src={service} className='h-7 w-7 ' />
-                      <h1 className='aboutUs text-xl'>{cita.servicio_id.nombre}</h1>
+                      <h1 className='aboutUs text-xl'>{citas.servicio_id.nombre}</h1>
+                      <img src={paid} className='h-7 w-7 ml-3' />
+                      <h1 className='aboutUs text-xl ml-3 text-red-600'>
+                        {citas.metodoPago}
+                      </h1>
                     </div>
                   </div>
                   <div className=" flex gap-3 dark:text-gray-400/100 text-lg items-center ">
@@ -151,19 +171,19 @@ export default function HairDresserPanel() {
                     </svg>
 
                     <div className="flex gap-4 items-center divide-x-2 ">
-                      <h1>{dayjs(cita.inicio).format('MMMM/DD/HH:mm')}</h1>
+                      <h1>{dayjs(citas.inicio).format('MMMM/DD/HH:mm')}</h1>
+
                       <div className="flex gap-2 pl-5 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 stroke-2">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <h1>{cita.status}</h1>
+                        <h1>{citas.status}</h1>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </>
-
           ))}
         </div>
       </div>
