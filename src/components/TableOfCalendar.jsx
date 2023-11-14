82f869f106fcb9b3b2834c2000149e1be3ba89d1
unknown <ospinopupo@gmail.com>
2023-11-14 16:21:40 -0500

    Haidresser panel finished

--- src/components/TableOfCalendar.jsx
+++ src/components/TableOfCalendar.jsx
@@ -1,9 +1,8 @@
-
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import '../TiltComponent.css';
 
-const TableOfCalendar = () => {
+const TableOfCalendar = ({ onDateChange }) => {
   const [date, setDate] = useState(new Date());
 
   const tileClassName = ({ date, view }) => {
@@ -11,25 +10,30 @@ const TableOfCalendar = () => {
     today.setHours(0, 0, 0, 0);
 
     return date.toISOString().split('T')[0] === today.toISOString().split('T')[0]
-    ? 'bg-purple-500 text-black font-bold' 
-    : null;
+      ? 'bg-purple-500 text-black font-bold'
+      : null;
   };
-  console.log(date)
- 
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+    // Solo llama a la función proporcionada por el padre si se proporciona
+    onDateChange && onDateChange(newDate);
+  };
+
+  /* console.log(date); */
 
   return (
     <>
-      <div className="max-w-md mx-auto p-4  ">
-      <Calendar
-        onChange={setDate}
-        value={date}
-        className="flex flex-col items-center "
-        tileClassName={tileClassName}
-      />
-    </div>
+      <div className="max-w-md mx-auto p-4 ">
+        <Calendar
+          onChange={handleDateChange}
+          value={date}
+          className="flex flex-col items-center"
+          tileClassName={tileClassName}
+        />
+      </div>
     </>
   );
 };
 
 export default TableOfCalendar;
-
