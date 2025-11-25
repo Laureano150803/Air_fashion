a0ffd03692ec2dfd3c9c7d0b37d197dcb908765d
lauda <laureanohurtado1@gmail.com>
2025-11-25 11:36:54 -0500

    main updatev2

--- api.js
+++ api.js
@@ -2,6 +2,9 @@ let apiUrl = 'http://localhost:8000/'
 
 if(import.meta.env.VITE_API === 'production'){
     apiUrl == import.meta.env.VITE_API
+    console.log(import.meta.env.VITE_API);
+}else{
+    console.log(import.meta.env.VITE_API);
 } 
 
 export default apiUrl
