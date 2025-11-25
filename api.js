c6af58816c9b0870d72e3746723c3664ef6bb9cf
lauda <laureanohurtado1@gmail.com>
2025-11-25 11:47:06 -0500

    main updatev4

--- api.js
+++ api.js
@@ -1,10 +1,9 @@
-let apiUrl = 'http://localhost:8000/'
+/* let apiUrl = 'http://localhost:8000/' */
+let apiUrl = 'https://airfashion.onrender.com/'
 
 if(import.meta.env.PROD){
     apiUrl == import.meta.env.VITE_API
     console.log("ambiente 1 "+import.meta.env.VITE_API);
-}else{
-    console.log("ambiente: "+import.meta.env.VITE_API);
-} 
+}
 
 export default apiUrl
